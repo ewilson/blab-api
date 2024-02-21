249c1bd83d8a04731bd931bfeb8048b5284e82c9
wilsonericn <wilson.eric.n@gmail.com>
2024-02-21 17:07:47 -0500

    First commit

---
+++ src/test/kotlin/com/honeyrain/blab/BlabApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.honeyrain.blab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
