3debf3dbb9ba049c4d5ab38d0555935dcbacdcb9
wilsonericn <wilson.eric.n@gmail.com>
2024-03-12 17:19:43 -0400

    extracted assembler

--- src/main/kotlin/com/honeyrain/blab/controllers.kt
+++ src/main/kotlin/com/honeyrain/blab/controllers.kt
@@ -3,22 +3,18 @@ package com.honeyrain.blab
 import org.slf4j.LoggerFactory
 import org.springframework.hateoas.CollectionModel
 import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.server.RepresentationModelAssembler
 import org.springframework.hateoas.server.mvc.linkTo
 import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
-class BlabController(val service: BlabService) {
+class BlabController(val service: BlabService, val assembler: BlabModelAssembler) {
     @GetMapping("/blab")
     fun all(): CollectionModel<EntityModel<Blab>> {
-        val blabs = service.findAll().stream().map { blab ->
-            EntityModel.of(blab,
-                    linkTo<BlabController> { one(blab.id!!) }.withSelfRel(),
-                    linkTo<BlabController> { all() }.withRel("blabs"),
-                    linkTo<UserController> { one(blab.userId) }.withRel("user"),
-            )
-        }.toList()
+        val blabs = service.findAll().stream().map(assembler::toModel).toList()
         return CollectionModel.of(blabs,
                 linkTo<BlabController> { all() }.withSelfRel(),
         )
@@ -27,12 +23,7 @@ class BlabController(val service: BlabService) {
     @GetMapping("/blab/{id}")
     fun one(@PathVariable id: String): EntityModel<Blab> {
         val blab = service.findById(id).orElseThrow { ObjectNotFoundException(id) }
-        return EntityModel.of(blab,
-                linkTo<BlabController> { one(id) }.withSelfRel(),
-                linkTo<BlabController> { all() }.withRel("blabs"),
-                linkTo<UserController> { one(blab.userId) }.withRel("user")
-        )
-    }
+        return assembler.toModel(blab)    }
 
     @DeleteMapping("/blab/{id}")
     fun delete(@PathVariable id: String) =
@@ -41,40 +32,25 @@ class BlabController(val service: BlabService) {
     @PostMapping("/blab")
     fun post(@RequestBody blab: Blab): EntityModel<Blab> {
         service.save(blab)
-        return EntityModel.of(blab,
-                linkTo<BlabController> { one(blab.id!!) }.withSelfRel(),
-                linkTo<BlabController> { all() }.withRel("blabs"),
-                linkTo<UserController> { one(blab.userId) }.withRel("user")
-        )
+        return assembler.toModel(blab)
     }
-
 }
 
 @RestController
-class UserController(val service: UserService) {
+class UserController(val service: UserService, val assembler: UserModelAssembler) {
 
     private val log = LoggerFactory.getLogger(javaClass)
     @GetMapping("/user")
     fun all(): CollectionModel<EntityModel<User>> {
-        val users = service.findAll().stream().map { user ->
-            EntityModel.of(user,
-                    linkTo<UserController> { one(user.id!!) }.withSelfRel(),
-                    linkTo<UserController> { all() }.withRel("users"),
-            )
-        }.toList()
-        return CollectionModel.of(users,
-                linkTo<UserController> { all() }.withSelfRel()
-        )
+        val users = service.findAll().stream().map(assembler::toModel).toList()
+        return CollectionModel.of(users, linkTo<UserController> { all() }.withSelfRel())
     }
 
 
     @GetMapping("/user/{id}")
     fun one(@PathVariable id: String): EntityModel<User> {
         val user = service.findById(id).orElseThrow { ObjectNotFoundException(id) }
-        return EntityModel.of(user,
-                linkTo<UserController> { one(id) }.withSelfRel(),
-                linkTo<UserController> { all() }.withRel("users"),
-        )
+        return assembler.toModel(user)
     }
     @DeleteMapping("/user/{id}")
     fun delete(@PathVariable id: String) {
@@ -84,6 +60,13 @@ class UserController(val service: UserService) {
     @PostMapping("/user")
     fun post(@RequestBody user: User): EntityModel<User> {
         service.save(user)
+        return assembler.toModel(user)
+    }
+}
+
+@Component
+class UserModelAssembler : RepresentationModelAssembler<User, EntityModel<User>> {
+    override fun toModel(user: User): EntityModel<User> {
         return EntityModel.of(user,
                 linkTo<UserController> { one(user.id!!) }.withSelfRel(),
                 linkTo<UserController> { all() }.withRel("users"),
@@ -91,14 +74,24 @@ class UserController(val service: UserService) {
     }
 }
 
+@Component
+class BlabModelAssembler : RepresentationModelAssembler<Blab, EntityModel<Blab>> {
+    override fun toModel(blab: Blab): EntityModel<Blab> {
+        return EntityModel.of(blab,
+                linkTo<BlabController> { one(blab.id!!) }.withSelfRel(),
+                linkTo<BlabController> { all() }.withRel("blabs"),
+                linkTo<UserController> { one(blab.userId) }.withRel("user"),
+        )
+    }
+}
+
+
 @ControllerAdvice
 internal class ObjectNotFoundAdvice {
     @ResponseBody
     @ExceptionHandler(ObjectNotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun objectNotFoundHandler(ex: ObjectNotFoundException): String? {
-        return ex.message
-    }
+    fun objectNotFoundHandler(ex: ObjectNotFoundException): String? = ex.message
 }
 internal class ObjectNotFoundException(id: String) : RuntimeException("Could not find object $id")
 
